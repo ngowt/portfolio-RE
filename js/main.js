60785cefb7da1bbfc57d72bf3215014368f7e864
Wilton Ngo <wiltonngo@gmail.com>
2019-09-03 15:29:59 -0400

    Add particlesjs

--- js/main.js
+++ js/main.js
@@ -1,31 +1,41 @@
-let currentRole = 0;
-const changeJob = () => {
-  const roles = [
-    "frontend",
-    "backend",
-    "fullstack",
-    "ui design",
-    "software development",
-    "game development"
-  ];
-  const randIndex = Math.floor(Math.random() * roles.length);
-  if (randIndex === currentRole) {
-    return changeJob();
-  }
-  currentRole = randIndex;
-  return roles[randIndex];
+window.onload = () => {
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load("particles-js", "assets/particles.json", () => {
+    console.log("callback - particles.js config loaded");
+  });
+
+  let currentRole = 0;
+  const changeJob = () => {
+    const roles = [
+      "frontend",
+      "backend",
+      "fullstack",
+      "ui design",
+      "software development",
+      "game development"
+    ];
+    const randIndex = Math.floor(Math.random() * roles.length);
+    if (randIndex === currentRole) {
+      return changeJob();
+    }
+    currentRole = randIndex;
+    return roles[randIndex];
+  };
+
+  setTimeout(
+    () =>
+      setInterval(() => {
+        document.getElementsByClassName(
+          "heading-primary--sub"
+        )[0].textContent = changeJob();
+      }, 5000),
+    100
+  );
+
+  document.getElementById("btn-home").onclick = () =>
+    scrollPageTo("#home", 1000);
+  document.getElementById("btn-about").onclick = () =>
+    scrollPageTo("#about", 1000);
+  document.getElementById("btn-projects").onclick = () =>
+    scrollPageTo("#projects", 1000);
 };
-
-setTimeout(
-  () =>
-    setInterval(() => {
-      document.getElementsByClassName(
-        "heading-primary--sub"
-      )[0].textContent = changeJob();
-    }, 5000),
-  100
-);
-
-document.getElementById('btn-home').onclick = () => scrollPageTo("#home", 1000);
-document.getElementById('btn-about').onclick = () => scrollPageTo("#about", 1000);
-document.getElementById('btn-projects').onclick = () => scrollPageTo("#projects", 1000);
